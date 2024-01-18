22ad95400ddcb528797ac2e2c12473b87fff89f4
YourDed4dey <angeldavila3828@gmail.com>
2024-01-18 15:41:32 -0600

    Gemmy Boss Bag Height Correction Commit

--- DedsQOLMod/obj/Debug/net6.0/DedsQOLMod.AssemblyInfo.cs
+++ DedsQOLMod/obj/Debug/net6.0/DedsQOLMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88982af6f8f16004bb8c72576ce30400ad0f2ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6237655cb4f7cbac8053de6c68063fec5a94c84")]
 [assembly: System.Reflection.AssemblyProductAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
