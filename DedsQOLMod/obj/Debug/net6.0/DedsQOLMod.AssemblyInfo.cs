12b4008746bac5aa6f0392c24205be2c67b5b1a9
YourDed4dey <angeldavila3828@gmail.com>
2024-01-18 19:19:26 -0600

    Updated Testing Commit

--- DedsQOLMod/obj/Debug/net6.0/DedsQOLMod.AssemblyInfo.cs
+++ DedsQOLMod/obj/Debug/net6.0/DedsQOLMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ad95400ddcb528797ac2e2c12473b87fff89f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef745192ea27628069731f379f2179a5c060e33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DedsQOLMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
